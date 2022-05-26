3a7fc575e8c58da10fe2b6af744c5a94e817bdd6
Martin Rokyta <martin.rokyta@seznam.cz>
2022-05-27 01:20:36 +0200

    Add CommonTime start getter and setter

--- src/main/java/cz/agents/map4rt/CommonTime.java
+++ src/main/java/cz/agents/map4rt/CommonTime.java
@@ -1,9 +1,17 @@
 package cz.agents.map4rt;
 
 public class CommonTime {
-	static long startedAt = System.currentTimeMillis(); 
+	static long startedAt = System.currentTimeMillis();
 	
 	public static int currentTimeMs() {
 		return (int) (System.currentTimeMillis() - startedAt);
 	}
+
+  public static void syncTime(long newStartedAt) {
+    startedAt = newStartedAt;
+  }
+
+  public static long getStartedAt() {
+    return startedAt;
+  }
 }
